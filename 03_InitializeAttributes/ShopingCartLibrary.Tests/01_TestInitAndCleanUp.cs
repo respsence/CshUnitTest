f3c8c4e0e1ed2d61ce6cdd3f716980b9666e073f
RespSence <negmetov@gmail.com>
2017-08-12 11:41:43 +0300

    Project UnitTest with Init and CleanUp data for TestMethod

---
+++ 03_InitializeAttributes/ShopingCartLibrary.Tests/01_TestInitAndCleanUp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingCartLibrary;
+using System.Diagnostics;
+
+namespace ShopingCartLibrary.Tests
+{
+    [TestClass]
+    public class TestInitAndCleanUp
+    {
+        private ShoppingCart cart;
+        private Item item;
+
+        // Запускается перед стартом каждого тестирующего метода
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Debug.WriteLine("Test Initialize");
+            item = new Item();
+            item.Name = "Unit Test Video Lessons";
+            item.Quantity = 10;
+
+            cart = new ShoppingCart();
+            cart.Add(item);
+        }
+
+        // Запускается после завершения каждого тестирующего метода
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            Debug.WriteLine("Test CleanUp");
+            cart.Dispose();
+        }
+
+        [TestMethod]
+        public void ShopingCart_CheckOut_ContainsItem()
+        {
+            CollectionAssert.Contains(cart.Items, item);
+        }
+
+        [TestMethod]
+        public void ShopingCart_RemoveItem_Empty()
+        {
+            int expected = 0;
+
+            cart.Remove(0);
+
+            Assert.AreEqual(expected, cart.Count);
+        }
+    }
+}
---
+++ 03_InitializeAttributes/ShoppingCartLibrary/ShoppingCart.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartLibrary
+{
+    [Serializable]
+    public class ShoppingCart : IDisposable
+    {
+        public List<Item> Items = new List<Item>();
+
+        public int Count
+        {
+            get { return Items.Count; }
+        }
+
+        public void Add(Item item)
+        {
+            Items.Add(item);
+        }
+
+        public void Dispose()
+        {
+            // cleanup code
+        }
+
+        public void Remove(int index)
+        {
+            Items.RemoveAt(index);
+        }
+    }
+
+    [Serializable]
+    public class Item
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
