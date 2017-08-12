b8c43d6a7cce2d2a763c84c028f2b1a886a22794
RespSence <negmetov@gmail.com>
2017-08-12 13:41:24 +0300

    Project UnitTest with example TestContext

---
+++ 05_TestContextSamples/TestContextSamples/MyTestSample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestContextSamples
+{
+    [TestClass]
+    public class MyTestSample
+    {
+        // TestContext используется для хранения информации о текущем юнит тесте
+        // При тестировании веб сервисов хранит URL
+        // При тестировании ASP.NET приложений - предоставляет доступ к ASP странице
+        // При использовании Data Driven тестов предоставляет доступ к источнику данных
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestContext.WriteLine("TestRunDirectory {0}", TestContext.TestRunDirectory);
+            TestContext.WriteLine("TestName {0}", TestContext.TestName);
+            TestContext.WriteLine("CurrentTestOutcome {0}", TestContext.CurrentTestOutcome);
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            TestContext.WriteLine("TestName (CleanUp) {0}", TestContext.TestName);
+            TestContext.WriteLine("CurrentTestOutcome (Cleanup) {0}", TestContext.CurrentTestOutcome);
+        }
+    }
+}
