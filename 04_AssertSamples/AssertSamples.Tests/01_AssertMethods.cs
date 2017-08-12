280760ea742c19d7992ef1c8276e6b3741a9f4d9
RespSence <negmetov@gmail.com>
2017-08-12 12:09:54 +0300

    Project UnitTest with example AssertMethod

---
+++ 04_AssertSamples/AssertSamples.Tests/01_AssertMethods.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AssertSamples.Tests
+{
+    [TestClass]
+    public class MyClassTests
+    {
+        [TestMethod]
+        public void IsSqrtTest()
+        {
+            // arrange
+            const double input = 4;
+            const double expected = 2;
+
+            // act
+            double actual = MyClass.GetSqrt(input);
+
+            // assert
+            Assert.AreEqual(expected, actual, "Sqrt of {0} should have been {1}!", input, expected);
+
+        }
+
+        [TestMethod]
+        public void DeltaTest()
+        {
+            const double expected = 3.1;
+            const double delta = 0.07;
+
+            // 3.1622776601683795
+            // 0.062..
+            double actual = MyClass.GetSqrt(10);
+
+            // Проверка значений на равенство с учетом погрешности delta
+            Assert.AreEqual(expected, actual, delta, "fail message!!!");
+
+        }
+
+        [TestMethod]
+        public void StringAreEqualTest()
+        {
+            // arrange
+            const string input = "HELLO";
+            const string expected = "hello";
+
+            // act and assert
+            // третий параметр - игнорирование регистра
+            Assert.AreEqual(expected, input, true);
+
+        }
+
+        [TestMethod]
+        public void StringSameTest()
+        {
+            string a = "Hello";
+            string b = "Hello";
+
+            // проверка равенства ссылок
+            Assert.AreSame(a, b);
+        }
+
+        [TestMethod]
+        public void IntegersSameTest()
+        {
+            int i = 10;
+            int j = 10;
+
+            // проверка равенства ссылок
+            Assert.AreSame(i, j);
+        }
+    }
+}
---
+++ 04_AssertSamples/AssertSamples/MyClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssertSamples
+{
+
+    public class MyClass
+    {
+        public static double GetSqrt(double value)
+        {
+            return Math.Sqrt(value);
+        }
+
+        public string SayHello(string name)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException("Parameter name cannot be null!");
+            }
+
+            return "Hello " + name;
+        }
+    }
+}
