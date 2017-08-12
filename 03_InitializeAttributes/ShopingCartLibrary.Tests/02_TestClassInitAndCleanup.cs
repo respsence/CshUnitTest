0d3d5de25b42468dbb9873fe3bbc3cf78250c7c1
RespSence <negmetov@gmail.com>
2017-08-12 11:56:23 +0300

    add UnitTest with Init and CleanUp data for ClassTest

---
+++ 03_InitializeAttributes/ShopingCartLibrary.Tests/02_TestClassInitAndCleanup.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShoppingCartLibrary;
+using System.Diagnostics;
+
+namespace ShopingCartLibrary.Tests
+{
+    [TestClass]
+    public class ClassInitAndCleanup
+    {
+        private static ShoppingCart cart;
+        // Запускается перед стартом первого тестирующего метода (один раз для класса)
+        // Метод должен быть открытым, статическим и принимать параметр типа TestContext
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Debug.WriteLine("Class Initialize");
+            Item item = new Item();
+            item.Name = "Unit Test Video Lessons";
+            item.Quantity = 10;
+
+            cart = new ShoppingCart();
+            cart.Add(item);
+        }
+
+        // Запускается после завершения последнего тестирующего метода (один раз для класса)
+        // Метод должен быть открытым статическим и возвращать void
+        [ClassCleanup]
+        public static void TestCleanUp()
+        {
+            Debug.WriteLine("Class CleanUp");
+            cart.Dispose();
+        }
+
+        [TestMethod]
+        public void ShopingCart_AddToCart()
+        {
+            int expected = cart.Items.Count + 1;
+
+            cart.Add(new Item() { Name = "Test", Quantity = 1 });
+
+            Assert.AreEqual(expected, cart.Count);
+        }
+
+        [TestMethod]
+        public void ShopingCart_RemoveFromCart()
+        {
+            int expected = cart.Items.Count - 1;
+
+            cart.Remove(0);
+
+            Assert.AreEqual(expected, cart.Count);
+        }
+    }
+}
