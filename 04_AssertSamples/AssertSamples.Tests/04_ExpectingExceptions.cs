2bb5b9e7bc3086e15330df0885253c1395eaec4d
RespSence <negmetov@gmail.com>
2017-08-12 13:26:58 +0300

    Project UnitTest with example ExceptionTestMethod

---
+++ 04_AssertSamples/AssertSamples.Tests/04_ExpectingExceptions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AssertSamples.Tests
+{
+    [TestClass]
+    public class ExpectingExceptions
+    {
+        // ExpectedException -  тест будет успешным если в процессе выполнения будет выброшено исключение с типом ArgumentNullException
+        [ExpectedException(typeof(ArgumentNullException), "Exception was not thrown")]
+        [TestMethod]
+        public void MyClass_SayHello_Exception()
+        {
+            MyClass instance = new MyClass();
+            instance.SayHello(null);
+        }
+
+        [TestMethod]
+        public void MyClass_SayHello_ReturnDmitriy()
+        {
+            // arrange
+            MyClass instance = new MyClass();
+            string expected = "Hello Dmitriy";
+
+            // act
+            string actual = instance.SayHello("Dmitriy");
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
