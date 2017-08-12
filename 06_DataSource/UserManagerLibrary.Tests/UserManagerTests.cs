adc7690d45b7b1dde3a4d0769eaca3d2b1c36eb8
RespSence <negmetov@gmail.com>
2017-08-12 14:02:08 +0300

    Project UnitTest with example DataSource from XML

---
+++ 06_DataSource/UserManagerLibrary.Tests/UserManagerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UserManagerLibrary.Tests
+{
+    // Data Driven Test - тест, который позволяет использовать для запуска данные из внешнего источника.
+    // Тест запускается по одному разу на каждую запись из источника данных. Это позволяет проверить разные входные данных 
+    // используя один метод.
+    [TestClass]
+    public class UserManagerTests
+    {
+        public TestContext TestContext { get; set; }
+        private UserManager manager = new UserManager();
+
+        // DataSource - определение источника данных.
+        // 1 параметр - имя провайдера 
+        // 2 параметр - строка подключения или путь к файлу
+        // 3 параметр - имя таблицы или элемента в XML
+        // 4 параметр - как происходит доступ к записям из источника данных
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+            "testData.xml",
+            "User",
+            DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void UserManager_Add_FromXML()
+        {
+            string userId = Convert.ToString(TestContext.DataRow["userid"]);
+            string telephone = Convert.ToString(TestContext.DataRow["telephone"]);
+            string email = Convert.ToString(TestContext.DataRow["email"]);
+
+            bool result = manager.Add(userId, telephone, email);
+
+            Assert.IsTrue(result, "Пользователь не может быть создан");
+        }
+    }
+}
---
+++ 06_DataSource/UserManagerLibrary/UserManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UserManagerLibrary
+{
+    public class UserManager
+    {
+        public bool Add(string userId, string phone, string email)
+        {
+            if (userId.Length < 4)
+            {
+                throw new Exception("UserId должен быть больше 4 символов");
+            }
+
+            if (phone.Contains("a"))
+            {
+                throw new Exception("Телефон должен содержать только цифры");
+            }
+
+            if (!email.Contains("@"))
+            {
+                throw new Exception("Ошибка в email адресе");
+            }
+
+            // Логика сохранения данных 
+
+            return true;
+
+        }
+    }
+}
