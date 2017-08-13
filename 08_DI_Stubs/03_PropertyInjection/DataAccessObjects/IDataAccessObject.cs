48c0b62f62005c6a5118bf3d8937f166039e533e
RespSence <negmetov@gmail.com>
2017-08-13 16:45:46 +0300

    add project UnitTest with PropertyInjection

---
+++ 08_DI_Stubs/03_PropertyInjection/DataAccessObjects/IDataAccessObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace _03_PropertyInjection
+{
+    public interface IDataAccessObject
+    {
+        List<string> GetFiles();
+    }
+}
---
+++ 08_DI_Stubs/03_PropertyInjection/Tests/FileManagerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _03_PropertyInjection.Tests
+{
+    [TestClass]
+    public class FileManagerTests
+    {
+        [TestMethod]
+        public void FileManager_FindFileLogByName_3()
+        {
+            IDataAccessObject accessObject = new StubFileDataObject();
+            FileManager fileManager = new FileManager();
+            fileManager.DataAccessObject = accessObject;// Dependency Injection
+            string fileName = "main.log";
+
+            bool exists = fileManager.FindLogFile(fileName);
+
+            Assert.IsTrue(exists, "File {0} was not found.", fileName);
+        }
+    }
+}
